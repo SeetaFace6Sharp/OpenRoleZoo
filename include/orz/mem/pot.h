6e39e79bc8f77fb4fb4303ba86db7fa924799ebf
withsalt <sysrun@163.com>
2023-01-20 13:37:51 +0800

    fix: 优化编译错误

--- include/orz/mem/pot.h
+++ include/orz/mem/pot.h
@@ -7,10 +7,13 @@
 
 #include <mutex>
 #include <memory>
+#include <functional>
 
-namespace orz {
+namespace orz
+{
 
-    class Pot {
+    class Pot
+    {
     public:
         using allocator = std::function<std::shared_ptr<void>(size_t)>;
 
@@ -21,8 +24,9 @@ namespace orz {
 
         void *relloc(size_t _size);
 
-        template<typename T>
-        T *calloc(size_t _count, bool copy = false) {
+        template <typename T>
+        T *calloc(size_t _count, bool copy = false)
+        {
             if (copy)
                 return reinterpret_cast<T *>(this->relloc(sizeof(T) * _count));
             else
@@ -47,4 +51,4 @@ namespace orz {
     };
 }
 
-#endif //ORZ_MEM_POT_H
+#endif // ORZ_MEM_POT_H
